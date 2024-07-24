775d6f1d7e9fdc4f98d8566a9db28d2123201c22
Albert Herrando Moraira <albert.herrando.8@gmail.com>
2024-07-24 19:23:58 +0200

    Código 00 y 01 hecho por Albert

---
+++ Albert/00-helloworld.js
@@ -0,0 +1,27 @@
+// Esto es un comentario simple
+
+/* Esto es
+un comentario
+en varias líneas
+*/
+
+console.log("Hello world!")
+console.log('Hola mundo!')
+console.log(`Esto es otro hola mundo!`)
+
+// console.log("Hello world!")
+
+/*
+console.log("Hello world!")
+console.log('Hola mundo!')
+colsole.log(`Esto es otro hola mundo`)
+*/
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
---
+++ Albert/01-variables.js
@@ -0,0 +1,25 @@
+// VARIABLES
+
+// var  (NO RECOMENDABLE, ANTIGUA MANERA)
+
+var helloWorld = "Hello world!"
+console.log(helloWorld)
+
+helloWorld = "Modifiying Hello world!"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "Hello world 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "New Hello world 2!"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3 = "This is a constant!"
+console.log(helloWorld3)
+
+// Error
+// helloWorld3 = "Re-assigning creates an error!"
