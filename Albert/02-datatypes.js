ade74d50cb20be89255aa007052beaa060ca95ba
Albert Herrando Moraira <albert.herrando.8@gmail.com>
2024-07-25 11:44:02 +0200

    Albert 02-datatypes

---
+++ Albert/02-datatypes.js
@@ -0,0 +1,47 @@
+// Tipos de datos primitivos
+
+// Cadenas de texto (string)
+let myName = "Albert Herrando"
+let alias = 'AlbertHM'
+let email = `albert.herrando.8@gmail.com`
+
+// Números (number)
+let age = 28 // Entero
+let height = 1.85 // Decimal
+
+// Booleanos (boolean)
+let isTeacher = false
+let isStudent = true
+
+// Undefined
+let undefinedVariable
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(3252345234534523513451345134513451345345)
+let myBigInt2 = 3252345234534523513451345134513451345345n
+
+// Mostramos los tipos de datos
+console.log(myName)
+console.log(alias)
+console.log(email)
+
+console.log(age)
+console.log(height)
+
+console.log(isTeacher)
+console.log(isStudent)
+
+console.log(undefinedVariable)
+
+console.log(nullValue)
+
+console.log(mySymbol)
+
+console.log(myBigInt)
+console.log(myBigInt2)
